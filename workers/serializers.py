9cfb209d2fcf2ad03cc96fff51189ff0e8a7ac2d
Erik Khalimov <biomaks@gmail.com>
2017-10-15 21:35:34 -0500

    workers update

--- workers/serializers.py
+++ workers/serializers.py
@@ -3,15 +3,23 @@ from rest_framework import serializers
 from workers.models import Worker, WorkerType
 
 
+class WorkerTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkerType
+        fields = '__all__'
+
+
 class WorkerSerializer(serializers.ModelSerializer):
-    worker_type = serializers.SlugRelatedField(slug_field='worker_type_name', read_only=True)
+    worker_type = WorkerTypeSerializer(read_only=False)
 
     class Meta:
         model = Worker
         fields = '__all__'
 
-
-class WorkerTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WorkerType
-        fields = '__all__'
+    def update(self, instance, validated_data):
+        worker_type_data = validated_data.pop('worker_type')
+        worker_type = WorkerType.objects.get(**worker_type_data)
+        instance.name = validated_data['name']
+        instance.worker_type = worker_type
+        instance.save()
+        return instance
